0d4df455db44a152493425180226da25bba96304
root <lennartsteinhoff@gmx.de>
2023-03-17 13:49:47 +0000

    Fix time format bug

--- src/format.cpp
+++ src/format.cpp
@@ -11,8 +11,12 @@ using std::string;
 string Format::ElapsedTime(long seconds) {
   string ss, mm, hh, dd;
   ss = std::to_string(seconds % 60);
+  ss.insert(0, 2 - ss.length(), '0');
   mm = std::to_string((seconds / 60) % 60);
+  mm.insert(0, 2 - mm.length(), '0');
   hh = std::to_string((seconds / (60 * 60)) % 24);
+  hh.insert(0, 2 - hh.length(), '0');
   dd = std::to_string((seconds / (60 * 60 * 24)));
+//  dd.insert(0, 2 - dd.length(), '0');
   return string(dd + ":" + hh + ":" + mm + ":" + ss);
 }
